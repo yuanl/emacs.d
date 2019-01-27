b93f9e8d8065c51e4c8d23213d5424e5f2ae234f
YuanL.Lee <lyuanl@gmail.com>
2019-01-27 18:49:32 +0800

    add header and footer

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,5 +1,10 @@
+;;; init-local.el --- Yuanl's customisations -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
 (add-to-list 'custom-theme-load-path
              (expand-file-name "emacs-color-theme-solarized" user-emacs-directory))
 (load-theme 'solarized t)
 
 (provide 'init-local)
+;;; init-local.el ends here
