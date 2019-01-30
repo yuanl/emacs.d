117c9d7d72ad50763feecec2ba475452f0cf0ec1
Leonard <leonard.li@sony.com>
2019-01-30 10:37:12 +0800

    remove solarized theme

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,13 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
-(add-to-list 'custom-theme-load-path
-             (expand-file-name
-              "emacs-color-theme-solarized" user-emacs-directory))
+;; (add-to-list 'custom-theme-load-path
+;;              (expand-file-name
+;;               "emacs-color-theme-solarized" user-emacs-directory))
 
 (add-hook 'after-init-hook 'toggle-frame-maximized)
 
-(load-theme 'solarized t)
+;; (load-theme 'solarized t)
 
 (sanityinc/enable-ivy-flx-matching)
 
