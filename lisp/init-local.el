0549f95cc5612e558588cd680ba5d5cd342d86e3
Leonard <leonard.li@sony.com>
2019-02-02 10:04:27 +0800

    add pipenv.el

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -11,5 +11,11 @@
 (when (maybe-require-package 'ace-window)
   (global-set-key (kbd "M-o") 'ace-window))
 
+(when (maybe-require-package 'pipenv)
+  (add-hook 'python-mode-hook #'pipenv-mode)
+  (setq
+   pipenv-projectile-after-switch-function
+   #'pipenv-projectile-after-switch-extended))
+
 (provide 'init-local)
 ;;; init-local.el ends here
