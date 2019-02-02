0c9ec1c3871476e8cb0628aedd1515b00a26c061
Leonard <leonard.li@sony.com>
2019-02-02 10:04:12 +0800

    purge solarized theme

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,13 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-;; (add-to-list 'custom-theme-load-path
-;;              (expand-file-name
-;;               "emacs-color-theme-solarized" user-emacs-directory))
-
 (add-hook 'after-init-hook 'toggle-frame-maximized)
-
-;; (load-theme 'solarized t)
 
 (sanityinc/enable-ivy-flx-matching)
 
