a000d53fbf5e22148875e5b5ed4c0155e307ad14
Leonard <leonard.li@sony.com>
2019-09-04 09:28:49 +0800

    add: mu4e
    Signed-off-by: Leonard <leonard.li@sony.com>

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -13,5 +13,21 @@
 
 (setq tramp-default-method "ssh")
 
+(when (require 'mu4e)
+  (setq
+   mail-user-agent 'mu4e-user-agent
+   mu4e-get-mail-command "offlineimap"
+   mu4e-update-interval 300
+   mu4e-view-show-images t
+   )
+  (setq
+   user-full-name  "Leonard Li"
+   user-mail-address "leonard.li@sony.com"
+   mu4e-user-mail-address-list '("leonard.li@sony.com" "leonard.li@sonymobile.com")
+   )
+  )
+
+(global-set-key (kbd "s-t") 'eshell)
+
 (provide 'init-local)
 ;;; init-local.el ends here
