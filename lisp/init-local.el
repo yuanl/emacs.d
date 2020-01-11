283498831455942000a48ee0a795f00eaf750506
Yuanliang <yuanl.lee@gmail.com>
2020-01-11 13:34:32 +0800

    fix: EAF only support Linux

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -20,8 +20,9 @@
 
 (global-set-key (kbd "s-t") 'eshell)
 
-; clone eaf to ~/.emacs.d/site-lisp
-(require 'eaf nil 'noerror)
+                                        ; clone eaf to ~/.emacs.d/site-lisp
+(when (eq system-type "gnu/linux")
+  (require 'eaf nil 'noerror))
 
 (provide 'init-local)
 ;;; init-local.el ends here
